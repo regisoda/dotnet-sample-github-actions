9e5ff09b40bbf0823ddd70b71f962777bac325fe
OTK Sistemas <otk@OTKs-MacBook-Pro.local>
2020-06-14 19:42:05 -0300

    Sample.Tests Project

---
+++ Sample.Tests/Integrations/HomeControllerTest.cs
@@ -0,0 +1,33 @@
+﻿using SampleAPI;
+using Xunit;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Http;
+
+namespace Sample.Tests.Integrations
+{
+    public class HomeControllerTest : IClassFixture<WebApplicationFactory<Startup>>
+    {
+        private readonly HttpClient client;
+        private readonly WebApplicationFactory<Startup> factory;
+
+        public HomeControllerTest(WebApplicationFactory<Startup> factory)
+        {
+            client = factory.CreateClient(new WebApplicationFactoryClientOptions
+            {
+                AllowAutoRedirect = false
+            });
+            this.factory = factory;
+        }
+
+        [Fact]
+        public async Task GetReturnsOk_When_ValidateIsLiveEndpoint()
+        {
+            var isLiveEndpoint = "/healthy";
+            var response = await client.GetAsync(isLiveEndpoint);
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+
+    }
+}
